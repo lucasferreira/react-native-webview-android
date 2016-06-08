97d16a519568beab91c95be0e489675138f6395c
Lucas Ferreira <panchorf@gmail.com>
2016-06-08 15:07:47 -0300

    - Change iOS component handle

--- index.ios.js
+++ index.ios.js
@@ -3,7 +3,36 @@
  */
 'use strict';
 
-var { UnimplementedView } = require('react-native');
-var RNWebViewAndroid = UnimplementedView;
+try {
+  var React = require('react');
+} catch(ex) {
+  var React = require('react-native');
+}
 
-module.exports = RNWebViewAndroid;
+var { StyleSheet } = require('react-native');
+
+var UnimplementedView = React.createClass({
+  setNativeProps: function() {
+    // Do nothing.
+  },
+  render: function() {
+    // Workaround require cycle from requireNativeComponent
+    var { View } = require('react-native');
+
+    return (
+      <View style={[styles.unimplementedView, this.props.style]}>
+        {this.props.children}
+      </View>
+    );
+  },
+});
+
+var styles = StyleSheet.create({
+  unimplementedView: {
+    borderWidth: 1,
+    borderColor: 'red',
+    alignSelf: 'flex-start',
+  }
+});
+
+module.exports = UnimplementedView;
